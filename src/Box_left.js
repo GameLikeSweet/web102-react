31b0f666e505a1e5d054a98ef071f02a323267fa
GameLikeSweet <tjdgns9978@gmail.com>
2025-02-04 19:45:48 +0900

    box module 250204

--- src/Box_left.js
+++ src/Box_left.js
@@ -2,10 +2,12 @@ import './about.css';
 import Box from './Box.js';
 
 
+let lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
 let Boxs = [];
 for(let i =0 ; i < 6; i++)
 {
-    Boxs.push(<Box hard1={i+1} para = 'text'/>);
+    Boxs.push(<Box hard1={i+1} para = {lorem}/>);
 }
 
 function box_left() {
